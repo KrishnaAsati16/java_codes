4cad3db90370426217fe8b815056b3aab5b8cfe4
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-11-09 14:08:23 +0530

    sum

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class sum {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 15;
+        int c = 188;
+        int sum = a + b + c;
+        System.out.println("Sum = " + sum);
+    
+
+
+    }
+            
+}
