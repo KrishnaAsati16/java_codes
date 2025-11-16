f476f9d8011e70bcbb40294a3ee8f8af0da64db7
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-11-16 12:24:47 +0530

    Builder

---
+++ lec5/program1.java
@@ -0,0 +1,26 @@
+package lec5;
+
+public class program1 {
+    public static void main(String[] args) {
+        String str = "i love java";
+        System.out.println(str);
+
+        String a[] = str.split(" ");
+
+        StringBuilder result=new StringBuilder();
+
+        for (String element : a) {
+            StringBuilder val = new StringBuilder(element);
+            result.append(val.reverse().append(" "));
+        }
+        System.out.println(result);
+        
+
+        // for (int i = 0; i < a.length; i++) {
+        //     a[i] = new StringBuilder(a[i]).reverse().toString();
+        //     String result = a[i];
+        //     System.out.println(result);
+        // }
+    }
+}
+    
