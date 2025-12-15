5e97646a13cbd59409a28486ddcfafe95d190ce9
Krishna Asati <krishnaasati2006@gmail.com>
2025-12-15 17:19:13 +0530

    Exception

---
+++ lec19/voting_age.java
@@ -0,0 +1,29 @@
+package lec19;
+
+public class voting_age {
+     public static void main(String[] args) {
+         try {
+             voteAge(15);
+
+         } catch (Exception e) {
+             System.out.println(e);
+         }
+         try {
+             voteAge(19);
+         } catch (Exception e) {
+             System.out.println(e);
+         }
+     }
+
+     static void voteAge(int age) {
+         System.out.println("Check for voting eligibility");
+         if (age < 18) {
+             throw new IllegalArgumentException("You cannot vote.");
+         }else{
+             System.out.println("You can vote");
+         }
+     }
+ }
+
+    
+
