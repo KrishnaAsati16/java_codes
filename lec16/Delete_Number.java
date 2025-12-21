306294370c429057ed6b23418aecd12fc5b444c0
Krishna Asati <krishnaasati2006@gmail.com>
2025-12-21 21:00:50 +0530

    HashSet

---
+++ lec16/Delete_Number.java
@@ -0,0 +1,20 @@
+package lec16;
+
+import java.util.HashSet;
+
+public class Delete_Number {
+     public static void main(String[] args) {
+        HashSet<Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+        set.add(6);
+        System.out.println("Set :"+set);
+        set.remove(2);
+        System.out.println("Set after Deleting 2:"+set);
+    }
+}
+    
+
